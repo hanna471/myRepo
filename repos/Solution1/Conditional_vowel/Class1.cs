75b1f0556b69d7c62feb122dadd04b4bffd8ff42
hanna471 <101120313+hanna471@users.noreply.github.com>
2022-05-24 21:30:50 +0700

    code project solution

---
+++ repos/Solution1/Conditional_vowel/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_vowel
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+
+            Console.WriteLine("enter a character");
+            ch = Console.ReadKey().KeyChar;
+            switch (ch)
+            {
+                case 'a':
+                case 'A':
+                case 'e':
+                case 'E':
+                case 'i':
+                case 'I':
+                case 'o':
+                case 'O':
+                case 'u':
+                case 'U':
+                    Console.WriteLine($"\n\t{ch} is a Vowel");
+
+                    break;
+                default:
+                    Console.WriteLine($"\n\t{ch} is a Consonant");
+                    break;
+
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
