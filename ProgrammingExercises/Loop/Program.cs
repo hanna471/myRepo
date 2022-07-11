137721e269fc1fac013dfa4dbe2f1fe6007569c5
hanna471 <101120313+hanna471@users.noreply.github.com>
2022-07-11 21:46:38 +0700

    change

---
+++ ProgrammingExercises/Loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new exercise4().alphabet();
+            new exercise4().ASCIIvalues();
+            new exercise4().table();
+            new exercise4().sum();
+            new exercise4().evenNumbers();
+            new exercise4().oddNumbers();
+            new exercise4().digit();
+            new exercise4().lastDigit();
+            new exercise4().number();
+        }
+    }
+}
